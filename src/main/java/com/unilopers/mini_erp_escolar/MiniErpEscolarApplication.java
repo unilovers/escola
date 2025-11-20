a34853ad0afdb0ab982efa74cc6537c9e7b97a71
Eduardo <eduardoluisalonsi@gmail.com>
2025-11-20 18:11:10 -0300

    :seedling: projeto inicial Spring Boot

---
+++ src/main/java/com/unilopers/mini_erp_escolar/MiniErpEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.unilopers.mini_erp_escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniErpEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniErpEscolarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unilopers/mini_erp_escolar/MiniErpEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilopers.mini_erp_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniErpEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
