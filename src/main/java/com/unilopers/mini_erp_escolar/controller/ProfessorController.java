4029c57ca6bae96256547678b7063340e45645e4
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 13:35:55 -0300

    :sparkles: implementar método CREATE no ProfessorController

--- src/main/java/com/unilopers/mini_erp_escolar/controller/ProfessorController.java
+++ src/main/java/com/unilopers/mini_erp_escolar/controller/ProfessorController.java
@@ -2,15 +2,33 @@ package com.unilopers.mini_erp_escolar.controller;
 
 import com.unilopers.mini_erp_escolar.dto.ProfessorRequestDTO;
 import com.unilopers.mini_erp_escolar.dto.ProfessorResponseDTO;
+import com.unilopers.mini_erp_escolar.model.Professor;
+import com.unilopers.mini_erp_escolar.repository.ProfessorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/professores")
 public class ProfessorController {
 
+    @Autowired
+    private ProfessorRepository professorRepository;
+
     @PostMapping
     public ProfessorResponseDTO criar(@RequestBody ProfessorRequestDTO dto) {
-        return null;
+        Professor professor = new Professor();
+        professor.setNome(dto.getNome());
+        professor.setEmail(dto.getEmail());
+
+        Professor salvo = professorRepository.save(professor);
+
+        ProfessorResponseDTO response = new ProfessorResponseDTO();
+        response.setId(salvo.getId());
+        response.setNome(salvo.getNome());
+        response.setEmail(salvo.getEmail());
+
+        return response;
     }
+
 
 }
