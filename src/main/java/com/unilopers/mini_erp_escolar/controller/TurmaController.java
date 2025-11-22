39020400f9b310ac78d58a5cbcae27bacb243609
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 14:15:22 -0300

    :sparkles: implementar o método CREATE no TurmaController

--- src/main/java/com/unilopers/mini_erp_escolar/controller/TurmaController.java
+++ src/main/java/com/unilopers/mini_erp_escolar/controller/TurmaController.java
@@ -1,17 +1,42 @@
 package com.unilopers.mini_erp_escolar.controller;
 
-import com.unilopers.mini_erp_escolar.dto.ProfessorResponseDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.unilopers.mini_erp_escolar.dto.TurmaRequestDTO;
+import com.unilopers.mini_erp_escolar.dto.TurmaResponseDTO;
+import com.unilopers.mini_erp_escolar.model.Professor;
+import com.unilopers.mini_erp_escolar.model.Turma;
+import com.unilopers.mini_erp_escolar.repository.ProfessorRepository;
+import com.unilopers.mini_erp_escolar.repository.TurmaRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/turmas")
 public class TurmaController {
 
-    @PostMapping
-    public ProfessorResponseDTO criar(@RequestBody ProfessorResponseDTO dto) {
-        return null;
+    private final TurmaRepository turmaRepository;
+    private final ProfessorRepository professorRepository;
+
+    public TurmaController(TurmaRepository turmaRepository, ProfessorRepository professorRepository) {
+        this.turmaRepository = turmaRepository;
+        this.professorRepository = professorRepository;
     }
+
+    // CREATE
+    @PostMapping
+    public ResponseEntity<TurmaResponseDTO> create(@RequestBody TurmaRequestDTO dto) {
+
+        Professor professor = professorRepository.findById(dto.getProfessorId())
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+
+        Turma turma = new Turma();
+        turma.setNome(dto.getNome());
+        turma.setProfessor(professor);
+
+        turmaRepository.save(turma);
+
+        return ResponseEntity.ok(toResponse(turma));
+    }
+
 }
