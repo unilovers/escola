0eae718bd1ca5c8c75ad42a4931f388c1f2f4e96
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 13:45:20 -0300

    :sparkles: Adicionar endpoint GET para listar professores, e professores por ID

--- src/main/java/com/unilopers/mini_erp_escolar/controller/ProfessorController.java
+++ src/main/java/com/unilopers/mini_erp_escolar/controller/ProfessorController.java
@@ -4,8 +4,11 @@ import com.unilopers.mini_erp_escolar.dto.ProfessorRequestDTO;
 import com.unilopers.mini_erp_escolar.dto.ProfessorResponseDTO;
 import com.unilopers.mini_erp_escolar.model.Professor;
 import com.unilopers.mini_erp_escolar.repository.ProfessorRepository;
+import org.aspectj.apache.bcel.classfile.annotation.RuntimeInvisTypeAnnos;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/professores")
@@ -29,6 +32,36 @@ public class ProfessorController {
 
         return response;
     }
+
+    //GET (lista todos)
+    @GetMapping
+    public List<ProfessorResponseDTO> listar() {
+        return professorRepository.findAll().stream()
+                .map(prof -> {
+                    ProfessorResponseDTO dto = new ProfessorResponseDTO();
+                    dto.setId((prof.getId()));
+                    dto.setNome(prof.getNome());
+                    dto.setEmail(prof.getEmail());
+                    return dto;
+                })
+                .toList();
+    }
+
+    //GET (por ID)
+    @GetMapping("/{id}")
+    public  ProfessorResponseDTO buscarPorId(@PathVariable Long id) {
+        Professor professor = professorRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Professor não encontrado"));
+
+        ProfessorResponseDTO dto = new ProfessorResponseDTO();
+        dto.setId(professor.getId());
+        dto.setNome(professor.getNome());
+        dto.setEmail(professor.getEmail());
+
+        return  dto;
+    }
+
+
 
 
 }
