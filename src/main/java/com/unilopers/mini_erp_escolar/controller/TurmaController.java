b658e5d3257d31317e0be5709347582d23821e39
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 14:11:30 -0300

    :sparkles: adicionar método CREATE no TurmaController

--- src/main/java/com/unilopers/mini_erp_escolar/controller/TurmaController.java
+++ src/main/java/com/unilopers/mini_erp_escolar/controller/TurmaController.java
@@ -1,4 +1,17 @@
 package com.unilopers.mini_erp_escolar.controller;
 
+import com.unilopers.mini_erp_escolar.dto.ProfessorResponseDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/turmas")
 public class TurmaController {
+
+    @PostMapping
+    public ProfessorResponseDTO criar(@RequestBody ProfessorResponseDTO dto) {
+        return null;
+    }
 }
