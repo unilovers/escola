3f5b5d31dd906920eb9c3cb54446461a475881c4
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 13:21:44 -0300

    :bug: corrigindo estrutura de classes do controller

--- src/main/java/com/unilopers/mini_erp_escolar/controller/Professor.java
+++
@@ -1,8 +0,0 @@
-package com.unilopers.mini_erp_escolar.controller;
-
-import org.springframework.web.bind.annotation.*;
-
-@RestController
-@RequestMapping("/professores")
-public class ProfessorController {
-}
