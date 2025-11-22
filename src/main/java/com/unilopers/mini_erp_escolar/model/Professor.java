e4183dbfda7d1daa7c8e8314620794b46fa3a8f4
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 11:21:15 -0300

    :sparkles: adicionar atributos ao model Professor

--- src/main/java/com/unilopers/mini_erp_escolar/model/Professor.java
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Professor.java
@@ -1,4 +1,21 @@
 package com.unilopers.mini_erp_escolar.model;
+import jakarta.persistence.*;
 
+import java.util.List;
+
+@Entity
 public class Professor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String email;
+
+    @OneToMany(mappedBy = "professor")
+    private List<Turma> turmas;
+
+
 }
