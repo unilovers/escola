63f77a68cd76812f198461f595b31802564fd1fb
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 11:55:51 -0300

    :sparkles: adicionar getters e setters ao model Professor

--- src/main/java/com/unilopers/mini_erp_escolar/model/Professor.java
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Professor.java
@@ -17,5 +17,35 @@ public class Professor {
     @OneToMany(mappedBy = "professor")
     private List<Turma> turmas;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Turma> getTurmas() {
+        return turmas;
+    }
+
+    public void setTurmas(List<Turma> turmas) {
+        this.turmas = turmas;
+    }
 }
