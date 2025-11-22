a74a79f0a35ab51fe6cdb31e1e2a74f9ef57461b
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 12:24:01 -0300

    :sparkles: adicionar atributos ao model Turmas

--- src/main/java/com/unilopers/mini_erp_escolar/model/Turma.java
+++ src/main/java/com/unilopers/mini_erp_escolar/model/Turma.java
@@ -1,4 +1,17 @@
 package com.unilopers.mini_erp_escolar.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Turma {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "professor_id")
+    private Professor professor;
 }
