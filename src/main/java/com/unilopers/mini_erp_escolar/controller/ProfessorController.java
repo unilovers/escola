0963ca461f3272b16e2482a5249be81995810224
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 13:23:19 -0300

    :sparkles: criar estrutura inicial do ProfessorController

--- src/main/java/com/unilopers/mini_erp_escolar/controller/ProfessorController.java
+++ src/main/java/com/unilopers/mini_erp_escolar/controller/ProfessorController.java
@@ -1,4 +1,8 @@
 package com.unilopers.mini_erp_escolar.controller;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/professores")
 public class ProfessorController {
 }
