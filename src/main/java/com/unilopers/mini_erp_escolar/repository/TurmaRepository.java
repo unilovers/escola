7f97d73fa3e86de7dba1e5a4e6351c2bca4d9107
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 12:55:13 -0300

    :building_construction: criar repositório de Turma

---
+++ src/main/java/com/unilopers/mini_erp_escolar/repository/TurmaRepository.java
@@ -0,0 +1,7 @@
+package com.unilopers.mini_erp_escolar.repository;
+
+import com.unilopers.mini_erp_escolar.model.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+}
