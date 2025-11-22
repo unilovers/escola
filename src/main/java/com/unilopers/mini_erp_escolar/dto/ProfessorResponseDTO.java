439bd5fccb057fdf2fc29db60b4b3469b6c66ef8
Juliana Furlan <julianafurlancosta@edu.unifil.br>
2025-11-22 12:59:06 -0300

    :building_construction: adicionar DTO de resposta de Professor

---
+++ src/main/java/com/unilopers/mini_erp_escolar/dto/ProfessorResponseDTO.java
@@ -0,0 +1,32 @@
+package com.unilopers.mini_erp_escolar.dto;
+
+public class ProfessorResponseDTO {
+
+    private Long id;
+    private String nome;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
